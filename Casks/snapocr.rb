6db4ef7722887b7d5e698459f7b915b5fb0dd864
Satoshi Morita <shiroemons@gmail.com>
2026-03-27 22:15:04 +0900

    chore: add SnapOCR cask definition

---
+++ Casks/snapocr.rb
@@ -0,0 +1,24 @@
+cask "snapocr" do
+  version "0.1.0"
+  sha256 "PLACEHOLDER"
+
+  url "https://github.com/shiroemons/snapocr/releases/download/v#{version}/SnapOCR-#{version}.dmg"
+  name "SnapOCR"
+  desc "OCR screen text via global hotkey and copy to clipboard"
+  homepage "https://github.com/shiroemons/snapocr"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :tahoe"
+
+  app "SnapOCR.app"
+
+  zap trash: [
+    "~/Library/Application Support/SnapOCR",
+    "~/Library/Preferences/com.shiroemons.snapocr.plist",
+    "~/Library/Caches/com.shiroemons.snapocr",
+  ]
+end
